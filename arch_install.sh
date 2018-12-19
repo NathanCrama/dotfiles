eb6055a752368d5ec5ded1f5d2f0cde7b427830c
Crama Nathan <nathancrama@gmail.com>
2018-12-19 12:09:17 +0100

    osos

--- arch_install.sh
+++ arch_install.sh
@@ -10,6 +10,8 @@ rm -rf ~/.vim
 echo "Deleting ~/dotfiles/"
 rm -rf ~/dotfiles
 
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 ln -s ~/dotfiles/.vim ~/.vim
 cat ~/dotfiles/.bashrc > ~/.bashrc
 cat ~/dotfiles/.bash_profile > ~/.bash_profile
@@ -17,7 +19,6 @@ cat ~/dotfiles/.vimrc > ~/.vimrc
 ln -s ~/dotfiles/darcula.vim ~/.vim/colors/darcula.vim 
 source ~/.bashrc
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
 echo "alias pacman='sudo pacman'" >> ~/.bashrc
