039aa88678277fd6ea4b6cd606878080d8bfd655
Crama Nathan <nathancrama@gmail.com>
2018-12-19 10:40:05 +0100

    osos

--- arch_install.sh
+++ arch_install.sh
@@ -6,7 +6,7 @@ sudo pacman --noconfirm base-devel git vim tree wget curl openssh network-manage
 curl -L https://raw.githubusercontent.com/NathanCrama/dotfiles/master/script.sh | sh && ~/.vim/bundle/YouCompleteMe/install.sh | grep 0
 
 echo "alias pacman='sudo pacman'" >> ~/.bashrc
-echo "alias pm='sudo pacman -Sy'" >> ~/.bashrc
+echo "alias pm='sudo pacman -Sy --noconfirm'" >> ~/.bashrc
 
 git clone https://aur.archlinux.org/package-query.git ~/package-query
 cd ~/package-query 
