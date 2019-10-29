4fe493e982d8ab46dc9a6eb16ab51f2896a4d00e
Nathan Crama <nathancrama@gmail.com>
2019-10-29 14:31:03 +0100

    aa

--- linux_misc/.rofi.sh
+++ linux_misc/.rofi.sh
@@ -1,7 +1,7 @@
 #!/bin/sh 
 
 bg_color=#1e1e1e
-text_color=#e44138
+text_color=#cf4f01
 htext_color=#ffffff
 
 rofi -show run -lines 5 -eh 2 -padding 40 -opacity "85" -bw 0 -color-window "$bg_color, $bg_color, $bg_color" -color-normal "$bg_color, $text_color, $bg_color, $bg_color, $htext_color" -font "Hasklig 13" -location 0 -separator-style none -hide-scrollbar
